bab09cdedd972b0dd644202f66d4355f6cd8f31c
ALyagova <aalyagova@gmail.com>
2026-08-22 22:05:43 +0300

    Fix PWA cache build

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles.css?v=31'
-import './cards.css?v=31'
+import './styles.css'
+import './cards.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js?v=31`).then((registration) => registration.update()))
